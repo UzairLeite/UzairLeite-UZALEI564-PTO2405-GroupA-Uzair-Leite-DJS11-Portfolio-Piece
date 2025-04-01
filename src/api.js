ae1a9c50d2b1e991621f27ddea7e4ab94686c743
UzairLeite <102726763+UzairLeite@users.noreply.github.com>
2025-04-01 18:47:12 +0200

    added api for previews and genres

---
+++ src/api.js
@@ -0,0 +1,15 @@
+import axios from "axios"
+const BASE_URL = "https://podcast-api.netlify.app"
+// /: Array of PREVIEW.
+// /genre/<ID>: GENRE object.
+// /id/<ID>: SHOW object with SEASON and EPISODE.
+
+export const getPreviews = async () => {
+    return axios.get(BASE_URL + "/")
+}
+
+export const getGenres = async (id) => {
+    const url = BASE_URL + "/genre/" + id
+    console.log(url)
+    return axios.get(url)
+}
