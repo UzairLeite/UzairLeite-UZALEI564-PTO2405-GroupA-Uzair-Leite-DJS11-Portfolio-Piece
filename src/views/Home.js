90ee7b73594863a93e410f8f9431097dfd8dedb3
UzairLeite <102726763+UzairLeite@users.noreply.github.com>
2025-04-01 22:16:43 +0200

    updated page

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,31 +1,36 @@
 import { getPreviews } from '../api';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Avatar, Button, Card } from "@chakra-ui/react"
 
 export function Home() {
+    const [previews, setPreviews] = useState([]);
     useEffect(() => {
         const fetchData = async () => {
-            const data = await getPreviews();
-            console.log("data", data)
+            const res = await getPreviews();
+            setPreviews(res.data)
+            console.log(res.data)
         }
         fetchData();
     }, []);
-    return <Card.Root width="320px">
-        <Card.Body gap="2">
-            <Avatar.Root size="lg" shape="rounded">
-                <Avatar.Image src="https://picsum.photos/200/300" />
-                <Avatar.Fallback name="Nue Camp" />
-            </Avatar.Root>
-            <Card.Title mt="2">Nue Camp</Card.Title>
-            <Card.Description>
-                This is the card body. Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit. Curabitur nec odio vel dui euismod fermentum.
-                Curabitur nec odio vel dui euismod fermentum.
-            </Card.Description>
-        </Card.Body>
-        <Card.Footer justifyContent="flex-end">
-            <Button variant="outline">View</Button>
-            <Button>Join</Button>
-        </Card.Footer>
-    </Card.Root>
+    return <div>
+        <h1>Podcasts</h1>
+        <div>
+            {previews.map((preview, index) => (
+                <Card.Root width="100%" key={index}>
+                    <Card.Body gap="2">
+                        <Avatar.Root size="lg" shape="rounded">
+                            <Avatar.Image src={preview.image} />
+                            <Avatar.Fallback name="Nue Camp" />
+                        </Avatar.Root>
+                        <Card.Title mt="2">{preview.title}</Card.Title>
+                        <Card.Description>{preview.description}</Card.Description>
+                    </Card.Body>
+                    <Card.Footer justifyContent="flex-end">
+                        <Button variant="outline">View</Button>
+                        <Button>Join</Button>
+                    </Card.Footer>
+                </Card.Root>
+            ))}
+        </div>
+    </div>
 }
